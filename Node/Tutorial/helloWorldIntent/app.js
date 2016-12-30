1ac6a1ea89b9e2db67479282baf17606b52a145d
rnjai <rnjailamba12@gmail.com>
2016-12-30 12:33:27 +0530

    basic regex intents

--- Node/Tutorial/helloWorldIntent/app.js
+++ Node/Tutorial/helloWorldIntent/app.js
@@ -2,7 +2,19 @@ var builder = require('botbuilder');
 
 var connector = new builder.ConsoleConnector().listen();
 var bot = new builder.UniversalBot(connector);
-bot.dialog('/', [
+var intents = new builder.IntentDialog();
+bot.dialog('/', intents);
+
+intents.matches(/^change name/i, [
+    function (session) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('Ok... Changed your name to %s', session.userData.name);
+    }
+]);
+
+intents.onDefault([
     function (session, args, next) {
         if (!session.userData.name) {
             session.beginDialog('/profile');
