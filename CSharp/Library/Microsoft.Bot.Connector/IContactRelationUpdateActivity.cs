b7a12ee517a6d20248f064373b872bebe92cce1d
rnjai <rnjailamba12@gmail.com>
2016-12-30 09:44:59 +0530

    first commit

---
+++ CSharp/Library/Microsoft.Bot.Connector/IContactRelationUpdateActivity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Microsoft.Bot.Connector
+{
+    /// <summary>
+    /// Someone has updated their contact list
+    /// </summary>
+    public interface IContactRelationUpdateActivity : IActivity
+    {
+
+        /// <summary>
+        /// add|remove
+        /// </summary>
+        string Action { get; set; }
+    }
+}
